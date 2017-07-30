b83263b8a2b970a28cac97222247802547842248
root <root@localhost.localdomain>
2017-07-30 02:14:35 -0400

    Added auth.py and auth2.py.

---
+++ auth.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+#auth.py
+from bs4 import BeautifulSoup
+import requests
+
+session = requests.Session()
+
+headers = {
+    "User-Agent": "Mozilla/5.0",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Language": "ja,en-US;q=0.7,en;q=0.3",
+    "Referer": "https://twitter.com/",
+    "Origin": "https://twitter.com",
+    "Upgrade-insecure-requests": "1"
+}
+
+response = session.get('htttps://twitter.com/', headers=headers, allow_redirects=True)
+soup = BeautifulSoup(response.text, "lxml")
+auth_token = soup.find(attrs={'name': 'authenticity_token'}).get('value')
+
+print "authenticity_token: " + auth_token
+
+
---
+++ auth2.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+#auth2.py
+from bs4 import BeautifulSoup
+import requests
+
+session = requests.Session()
